8df2dc580e11f874bffeef3b02a1ff093935c2cc
Douglas Ortiz <alexanderortiz333@gmaill.com>
2018-06-30 17:31:03 -0600

    Se finaliza el sistema de manera general

---
+++ Starmovies/Controllers/EstrenosController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Starmovies.Controllers
+{
+    public class EstrenosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DirtyDancing()
+        {
+            return View();
+        }
+
+        public IActionResult Grease()
+        {
+            return View();
+        }
+
+        public IActionResult FlashDance()
+        {
+            return View();
+        }
+
+        public IActionResult Saturday()
+        {
+            return View();
+        }
+    }
+}
